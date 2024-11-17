58e1b14f1681e8debad81a2a1790f16771b4fcd9
mb <mouxez@gmail.com>
2024-11-17 19:53:02 +0400

    update app.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,55 @@
-function App() {
-  return <div>App</div>;
-}
+import { FC, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { IRawData, updateVehicle } from './store/vehicleSlice';
+import SocketService from './services/socketService';
+import VehicleList from './components/VehicleList/VehicleList';
+import MapView from './components/MapView/MapView';
+import VehicleInfoPopup from './components/VehicleInfoPopup/VehicleInfoPopup';
+import { VEHICLES } from './const';
+import { RootState } from './store';
+import './App.scss';
+
+const App: FC = () => {
+  const dispatch = useDispatch();
+  const vehicles = useSelector((state: RootState) => state.vehicle.vehicles);
+  const selectedVehicle = useSelector((state: RootState) => state.vehicle.selectedVehicle);
+
+  useEffect(() => {
+    try {
+      SocketService.connect();
+    } catch (error) {
+      console.error('WebSocket connection error:', error);
+    }
+
+    VEHICLES.forEach((plate) => {
+      try {
+        SocketService.subscribeToVehicle(plate);
+      } catch (error) {
+        console.error(`Failed to subscribe to vehicle ${plate}:`, error);
+      }
+    });
+
+    SocketService.onVehicleData((rawData: IRawData) => {
+      const { plate, data } = rawData;
+      dispatch(updateVehicle({ ...data, plate }));
+    });
+
+    return () => {
+      SocketService.disconnect();
+    };
+  }, [dispatch]);
+
+  return (
+    <div className="app">
+      <div className="app__panel">
+        <VehicleList />
+      </div>
+      <main className="app__map">
+        <MapView />
+      </main>
+      {selectedVehicle && vehicles[selectedVehicle] && <VehicleInfoPopup />}
+    </div>
+  );
+};
 
 export default App;
